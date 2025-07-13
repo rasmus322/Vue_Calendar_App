57253218cf3b0b6c9e17dc57f23a00f8f4738602
rasmus322 <saltanovvlad@yandex.ru>
2025-07-13 11:48:31 +0500

    fix for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
-git push -f git@github.com:rasmus322/vue_calendar_app.git main:gh-pages
+git push -f https://github.com/rasmus322/vue_calendar_app.git main:gh-pages
 
 cd -
